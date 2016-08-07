b950651fc9049f6b7abeb77bcd076ebcea7cbc79
datagutten <datagutten@datagutten.net>
2016-08-07 20:46:28 +0200

    Updated database setup script

--- setup.sql
+++ setup.sql
@@ -3,8 +3,15 @@ CREATE TABLE `devices` (
   `mac` varchar(45) NOT NULL,
   `name` varchar(45) DEFAULT NULL,
   `description` varchar(100) DEFAULT NULL,
-  `video` varchar(150) DEFAULT NULL,
-  `message` varchar(100) DEFAULT NULL,
+  `message` varchar(200) DEFAULT NULL,
+  `ip` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `mac_UNIQUE` (`mac`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+CREATE TABLE `videos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `device` int(11) NOT NULL,
+  `file` varchar(150) NOT NULL,
+  `position` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
